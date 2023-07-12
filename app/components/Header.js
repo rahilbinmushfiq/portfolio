98209565490407527b5938e5d685163fa64f2855
Rahil Bin Mushfiq <rahilbinmushfiq@gmail.com>
2023-07-12 22:19:09 +0600

    style(Header): update header and its navbar styles
    This commit styles the header element to have a lighter box-shadow, and its desktop navigation links to have a semibold font-weight and lighter text color.

--- app/components/Header.js
+++ app/components/Header.js
@@ -189,7 +189,7 @@ export default function Header() {
   }, [pathname, router]);
 
   return (
-    <header className="fixed inset-0 -translate-y-24 invisible z-[2] flex flex-col h-20 p-6 bg-white bg-opacity-75 backdrop-blur-sm shadow-lg sm:px-12 md:px-16 lg:h-[86px] lg:px-20 xl:px-36 2xl:px-56">
+    <header className="fixed inset-0 -translate-y-24 invisible z-[2] flex flex-col h-20 p-6 bg-white bg-opacity-75 backdrop-blur-sm shadow-[0_6px_36px_0_rgba(0,0,0,0.075)] sm:px-12 md:px-16 lg:h-[86px] lg:px-20 xl:px-36 2xl:px-56">
       <div className="flex justify-between items-center -translate-y-3.5 invisible lg:items-center lg:font-medium">
         {/* Website Logo */}
         <div>
