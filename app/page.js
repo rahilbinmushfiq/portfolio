66d1900f953bea87fa44bc528bd92a8100ca491b
Rahil Bin Mushfiq <rahilbinmushfiq@gmail.com>
2023-07-27 01:55:47 +0600

    feat: implement metadata and SEO enhancements
    This commit adds metadata and SEO enhancements, including the page title, description, canonical URL, OpenGraph, and Twitter card data to all the pages in the app. The changes include:
    
    - Add a metadata object in the `Home` page component to provide SEO information for the homepage
    - Dynamically generate metadata for individual project pages based on the `projectId` parameter in the `Project Details` page component
    - Remove the metadata object initially created in the Next.js boilerplate from the root `Layout` component
    
    The metadata and SEO enhancements aim to improve search engine visibility and social media sharing for the app's pages.

--- app/page.js
+++ app/page.js
@@ -5,6 +5,44 @@ import About from "./components/pages/home/sections/About";
 import Portfolio from "./components/pages/home/sections/Portfolio";
 import Contact from "./components/pages/home/sections/Contact";
 
+export const metadata = {
+  title: 'Rahil Bin Mushfiq | Frontend Developer',
+  description: 'I\'m Rahil Bin Mushfiq, a frontend developer who loves crafting unique and intuitive web experiences with a focus on clean code and user-centered design.',
+  alternates: {
+    canonical: `https://rahilbinmushfiq.vercel.app`,
+  },
+  openGraph: {
+    title: 'Rahil Bin Mushfiq | Frontend Developer',
+    description: 'I\'m Rahil Bin Mushfiq, a frontend developer who loves crafting unique and intuitive web experiences with a focus on clean code and user-centered design.',
+    url: 'https://rahilbinmushfiq.vercel.app',
+    siteName: 'Rahil Bin Mushfiq',
+    images: [
+      {
+        url: 'https://www.dropbox.com/scl/fi/2k9nmpqvln1sty6z4ywjp/home.png?rlkey=dt8cd1wjb9fmzd5c4s28rww1g&raw=1',
+        width: 900,
+        height: 600,
+        alt: 'developer coding',
+      },
+    ],
+    type: 'website',
+  },
+  twitter: {
+    card: 'summary_large_image',
+    title: 'Rahil Bin Mushfiq | Frontend Developer',
+    description: 'I\'m Rahil Bin Mushfiq, a frontend developer who loves crafting unique and intuitive web experiences with a focus on clean code and user-centered design.',
+    domain: 'https://rahilbinmushfiq.vercel.app',
+    url: `https://rahilbinmushfiq.vercel.app`,
+    images: [
+      {
+        url: 'https://www.dropbox.com/scl/fi/2k9nmpqvln1sty6z4ywjp/home.png?rlkey=dt8cd1wjb9fmzd5c4s28rww1g&raw=1',
+        width: 900,
+        height: 600,
+        alt: 'developer coding',
+      },
+    ],
+  },
+};
+
 export default function Home() {
   return (
     <Main >
--- app/project/[projectId]/page.js
+++ app/project/[projectId]/page.js
@@ -1,6 +1,55 @@
 import Article from "@/app/components/pages/projectDetails/Article";
 import { projects } from "@/app/data/projects";
 
+export function generateMetadata({ params: { projectId } }) {
+  const project = projects[Number(projectId)];
+
+  if (!project) {
+    return {
+      title: 'Unknown Project | Rahil Bin Mushfiq',
+      description: 'The requested project is not found. Please visit the Portfolio section of the homepage.',
+    };
+  } else {
+    return {
+      title: `${project.title} | Project | Rahil Bin Mushfiq`,
+      description: project.summary,
+      alternates: {
+        canonical: `https://rahilbinmushfiq.vercel.app/project/${projectId}`,
+      },
+      openGraph: {
+        title: `${project.title} | Project | Rahil Bin Mushfiq`,
+        description: project.summary,
+        url: `https://rahilbinmushfiq.vercel.app/project/${projectId}`,
+        siteName: 'Rahil Bin Mushfiq',
+        images: [
+          {
+            url: project.openGraphImage,
+            width: 900,
+            height: 600,
+            alt: `${project.title} Project Mockup`,
+          },
+        ],
+        type: 'article',
+      },
+      twitter: {
+        card: 'summary_large_image',
+        title: `${project.title} | Project | Rahil Bin Mushfiq`,
+        description: project.summary,
+        domain: 'https://rahilbinmushfiq.vercel.app',
+        url: `https://rahilbinmushfiq.vercel.app/project/${projectId}`,
+        images: [
+          {
+            url: project.openGraphImage,
+            width: 900,
+            height: 600,
+            alt: `${project.title} Project Mockup`,
+          },
+        ],
+      },
+    };
+  }
+}
+
 export const dynamicParams = false;
 
 export function generateStaticParams() {
